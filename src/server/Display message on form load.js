0e253f07306576497e377f1fed5ca7460a463e50
System Administrator <admin@example.com>
2025-04-04 19:50:52 -0700

    commit again

---
+++ src/server/Display message on form load.js
@@ -0,0 +1,10 @@
+import { gs } from '@servicenow/glide'
+
+export function showStateUpdate(current, previous) { 
+    const currentState = current.getValue('state')
+    const previousState = previous.getValue('state')
+
+    gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
+    gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
+    gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
+}
